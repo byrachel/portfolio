a29e9082dd0ebb8ef0371756072012df6360f202
Rachel Nething <byrachel@gmail.com>
2020-03-13 16:20:48 +0100

    webdesign page created

---
+++ webdesign.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Développeur Web & Mobile : Rachel Nething</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+  <link href="https://fonts.googleapis.com/css?family=Alata&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    <header>
+        
+        <?php include("menu.php"); ?>
+            
+    </header>
+
+    <div class="projet container">
+        <div id="up"></div>
+        <br />
+        <p class="date align-right"><a href="karroad.php">Karroad (MERN) >></a></p>
+        <h1 class="projet-title center">Développeur web & mobile</h1>
+        <h2 class="center">Webdesign</h2>
+        <br />
+        <div class="underline"></div>
+    </div>
+    <section>
+
+        <div class="container img-container center pb-5">
+            <p class="center">Création graphique <span class="slogan"> - UI design - </span> d'une application web de formation en ligne dédiée au web marketing. La page d'accueil incite l'utilisateur à se connecter à son interface, il peut ensuite soit accéder à un cursus de formation, soit effectuer une recherche par mot clés pour sélectionner un sujet précis.</p>
+            <br />
+            <img src="img/macoma.jpg" alt="Création d'une interface utilisateur (UI/UX design)" class="img-fluid" width="800px"/>
+        </div>
+
+        <div class="container mt-2">
+            <div class="row">
+                <div class="col-md-7">
+                    <img src="img/ui-user-interface.png" class="img-fluid shadow"/>
+                </div>
+                <div class="col-md-5">
+                    <br />
+                    <p class="slogan">Objectifs :</p>
+                    <br />
+                        <ul>
+                            <li><p>Respecter la charte graphique</p></li>
+                            <li><p>Attirer le visiteur vers les boutons d'appel à l'action (CTA)</p></li>
+                            <li><p>Créer un <strong>parcours utilisateur</strong> fluide</p></li>
+                            <li><p>Imaginer une <strong>expérience utilisateur</strong> agréable (UX)</p></li>
+                            <li><p>Utiliser le logiciel Invision</p></li>
+                            <li><p>Optimiser le contenu pour un usage web ou mobile (responsive)</p></li>
+                            <li><p>Minimiser le "bruit" visuel et privilégier une <strong>ergonomie facilitatrice, moderne et épurée</strong></p></li>
+                        </ul>
+                    <br />
+                </div>
+            </div>
+        </div>
+
+        <div class="divider center">
+            <img src="img/macoma-preview.png" class="img-fluid" width="700px" />
+            <div class="divider"></div>
+            <a href="#up"><img src="img/up.png" class="right" /></a>
+        </div>
+
+    </section>
+    <div class="divider"></div>
+    <footer>
+        <div class="container-fluid yellow-footer">
+
+            <p class="center white">© Website réalisé en HTML5 / CSS3 / Javascript.</p>
+
+        </div>
+    </footer>
+
+</body>
+
+</html>
